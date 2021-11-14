1f08a1bc1f6c9c3bea05af31a17e72756ae7ff7a
itamar-sh <itamar.schechter@gmail.com>
2021-11-15 00:44:14 +0200

    order

---
+++ Greedy Algorithms/adjusted_graph.py
@@ -0,0 +1,9 @@
+# Helper class represents Adjusted Graph
+
+class Vertex:
+    weight = 0.0
+    list_of_edges = list()
+
+
+class Graph:
+    list_of_vertices = list()
---
+++ Greedy Algorithms/all_pairs_shortest_paths.py
@@ -0,0 +1,20 @@
+# Taken from practice 5
+
+"""
+Input:
+Adjusted graph: int. represents the length of the rail.
+connectors: int. All the kinds of connectors. each kind of connector represented by a number.
+parts : list[list[int]]. Every part is a list of 4 int.
+The first int is the kind of connector in the beginning.
+The second int is the kind of connector in the end.
+the third int is the length of the current part.
+the fourth int is the cost of the current part.
+
+Output:
+int: the cost of legal rail in length of rail_len.
+Legal rail is a rail that all the linked parts are connected with the same connectors.
+"""
+from adjusted_graph import *
+
+def all_pairs_shortest_paths(graph: Graph, weight_func: callable[Vertex], parts: list[list[int]]) -> int:
+    return 1
