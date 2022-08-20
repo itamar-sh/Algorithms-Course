5654df70b1604fba624dbe84af940c006c8a7f1c
Itamar Shechter <84578656+itamar-sh@users.noreply.github.com>
2022-08-20 22:03:10 +0300

    Assignment Of Independent Vector Set With Maximal Weight Problem - Not finished

---
+++ Greedy Algorithms/Vectors Set With Maximal Weight Problem/Vector Set With Maximal Weight Solution in python.py
@@ -0,0 +1,6 @@
+from typing import List, Callable
+
+
+class Vectors_Set_With_Maximal_Weight_Solution:
+    def main_solution(self, vectors: List[Vector], weight_func: Callable[Vector, int]) -> List[Vector]:
+        pass
